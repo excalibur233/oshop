0f87899f2f67162dd98417a2231d31531a7962dd
蔡乾 <cai.q@foxmail.com>
2017-07-05 14:03:18 +0800

    finish mix config

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 use OShop\Core\Wechat\RoutesHandler;
 
 Route::get('/', function () {
-    dd(Auth::user()->wechatWebProfile);
     return view('welcome');
 });
 
