aee27d5b512b0021b67a8636993101008e52d87f
fishyu <501254061@qq.com>
2017-12-12 18:06:37 +0800

    debug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 
 <!-- Scripts -->
 
-<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript" charset="utf-8">
     wx.config(<?php echo app('wechat.official_account')->jssdk->buildConfig(array('openAddress'), true) ?>);
 </script>
